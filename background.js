818c61c1de9e24e9fc29b5f750b4685334d90573
Heil_Kaizen <84093517+heil-kaizen@users.noreply.github.com>
2025-12-22 22:37:20 +0600

    Refactor background.js for cleaner event handling

--- background.js
+++ background.js
@@ -1,21 +1,15 @@
-// 1. Setup the 2-minute alarm on installation
 chrome.runtime.onInstalled.addListener(() => {
   chrome.alarms.create("alonQuoteAlarm", { periodInMinutes: 2 });
 });
 
-// 2. Helper function to send message to the active tab
 function triggerAlonQuote(tabId) {
-  chrome.tabs.sendMessage(tabId, { action: "show_quote" }).catch((err) => {
-    console.log("Waiting for page to be ready for Alon...");
-  });
+  chrome.tabs.sendMessage(tabId, { action: "show_quote" }).catch(() => {});
 }
 
-// 3. Handle manual icon clicks
 chrome.action.onClicked.addListener((tab) => {
-  triggerAlonQuote(tab.id);
+    if (tab.id) triggerAlonQuote(tab.id);
 });
 
-// 4. Handle automated alarm (Every 2 Minutes)
 chrome.alarms.onAlarm.addListener((alarm) => {
   if (alarm.name === "alonQuoteAlarm") {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
